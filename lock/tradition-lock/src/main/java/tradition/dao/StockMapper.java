2db4149210083c11c41c04da8ab7b13e2538c7ac
PengFuLin <1162855925@qq.com>
2023-02-03 01:40:21 +0800

    v1.0.3  -加入redis手动实现锁机制

--- lock/tradition-lock/src/main/java/tradition/dao/StockMapper.java
+++ lock/tradition-lock/src/main/java/tradition/dao/StockMapper.java
@@ -7,10 +7,8 @@ import org.apache.ibatis.annotations.Update;
 
 public interface StockMapper extends BaseMapper<Stock> {
 
-
     @Update("update db_stock set count=count -#{count} where product_code = #{productCode} and count >= #{count}")
     int updateStock (@Param("productCode") String productCode,@Param("count") Integer count);
-
 
     Stock selectStockForUpdate(Long id);
 }
